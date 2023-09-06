63ff38a7c13b25766765f44819fa4c7379613f37
Lucas Marques <94138717+LuscaM@users.noreply.github.com>
2023-09-06 20:31:46 -0300

    Update Login
    Atualizações no Codigo

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import { SafeAreaView, StyleSheet, TextInput, TouchableOpacity, Text } from "react-native";
+import { SafeAreaView, StyleSheet, TextInput, Button, TouchableOpacity } from "react-native";
 import Home from "./Home";
+import App from "../../App";
 
 const style = StyleSheet.create({
     container: {
@@ -21,25 +22,25 @@ const style = StyleSheet.create({
     btnLogar: {
         width: 100,
         height: 40,
-        backgroundColor: '#9e09de',
+        backgroundColor: '#b7ff00',
         borderRadius: 10,
         alignItems: 'center',
         justifyContent: 'center',
+        color: "#fff"
+
 
     }
 })
 
-const btnCad = () => {
-    <Home />
-}
 
 
-const Login = () => {
+const Login = ({ navigation }: any) => {
 
     const [login, setLogin] = useState<string>();
     const [senha, setSenha] = useState<string>();
     const [hasError, setHasError] = useState(false);
 
+    // Setando o Login
     const onChangeInput = (value: string) => {
         if (value.length <= 0) {
             setHasError(true)
@@ -47,6 +48,16 @@ const Login = () => {
             setHasError(false)
         }
         setLogin(value)
+
+    };
+
+    // Setando a senha
+    const onChangeInputSenha = (value: string) => {
+        if (value.length <= 0) {
+            setHasError(true)
+        } else {
+            setHasError(false)
+        }
         setSenha(value)
     };
 
@@ -56,10 +67,12 @@ const Login = () => {
             <TextInput placeholder="Digite seu Login" style={style.textInput} value={login} onChangeText={onChangeInput} />
             {hasError ?? null}
 
-            <TextInput secureTextEntry={true} placeholder="Digite sua Senha" style={style.textInput} value={senha} onChangeText={onChangeInput} />
+            <TextInput secureTextEntry={true} placeholder="Digite sua Senha" style={style.textInput} value={senha} onChangeText={onChangeInputSenha} />
 
-            <TouchableOpacity style={style.btnLogar} onPress={btnCad}>
-                <Text>Logar</Text>
+            <TouchableOpacity style={style.btnLogar}>
+                <Button onPress={() => {
+                    navigation.navigate('Home')
+                }} title="Logar" />
             </TouchableOpacity>
 
         </SafeAreaView>
